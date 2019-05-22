721cf53bb2b292480f193394ac338b7e17a867db
minoru.kamada <minbooo185@gmail.com>
2019-05-22 10:43:59 +0000

    references destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,5 +4,5 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   
   has_many :reverses_of_favorite, class_name: 'Favorite'
-  has_many :likers, through: :reverses_of_favorite, source: :user
+  has_many :likers, through: :reverses_of_favorite, source: :user, dependent: :destroy
 end
